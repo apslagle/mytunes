515d0475585183dffdaaa8074bc95e517ed64dd1
Andrew Slagle <apslagle@gmail.com>
2017-01-05 12:44:04 -0600

    songModel songQueue functionality

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,17 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    // console.log('This collection: ' + this);
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    }, this);
+  },
+
+  playFirst: function() {
+    // console.log(this.at(0));
+    this.at(0).play();
   }
 
 });
